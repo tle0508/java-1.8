f114b6360cbc27ad2d9da23a00130dbc31dd8867
sirun sununtasin <tlezazazazaza@gmail.com>
2024-02-15 16:33:38 +0700

    test

--- abc.java
+++ abc.java
@@ -1,15 +1,5 @@
 public class abc {
     public void test(){
-        try{
-            AutoCloseable ac=null;
-        }catch (Exception e){
-
-        }
-
-        try(AutoCloseable ac=null){
-
-        }catch (Exception e){
-
-        }
+    System.out.println("asdsd");
     }
 }
